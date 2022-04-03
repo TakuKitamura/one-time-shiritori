ff7b76ef3cfb9a506dd8eb8dd4cc56dd186cae39
TakuKitamura <takukitamura.io@gmail.com>
2022-04-03 21:17:35 +0900

    add an implement and test codes to set a first world and get the words

--- contracts/Shiritori.sol
+++ contracts/Shiritori.sol
@@ -1,7 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Shiritori {
-  constructor() public {
-  }
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Shiritori is Ownable {
+    string private _history = "";
+
+    // get shiritori history
+    function history() external view returns (string memory) {
+        return _history;
+    }
+
+    // contract-owner can set shiritori-history only once
+    function setFirstWord(string calldata word) external onlyOwner {
+        bool isEmptyWord = bytes(_history).length == 0;
+        if (isEmptyWord) {
+            // set first word on storage
+            _history = word;
+        }
+    }
 }
--- test/shiritori_test.js
+++ test/shiritori_test.js
@@ -5,11 +5,86 @@ const Shiritori = artifacts.require("Shiritori");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("ShiritoriTest", function ( /* accounts */ ) {
+contract("ShiritoriTest", function (accounts) {
+    const ownerAddress = accounts[0];
+    const notOwnerAddress = accounts[1];
     describe("deployment", () => {
         it("has been deployed successfully", async () => {
             const shiritori = await Shiritori.deployed();
             assert(shiritori, "contract failed to deploy");
         });
+    });
+
+    describe("when word is set by the owner", () => {
+        it("set first word", async () => {
+            const shiritori = await Shiritori.deployed()
+            const firstWord = "しりとり";
+            await shiritori.setFirstWord(firstWord, {
+                from: ownerAddress
+            });
+            const history = await shiritori.history({
+                from: ownerAddress
+            });
+
+            assert.equal(history, firstWord, "first word was not set");
+        });
+
+        it("call setFirstWord twice", async () => {
+            const shiritori = await Shiritori.deployed()
+            const firstWord = "しりとり";
+            await shiritori.setFirstWord(firstWord, {
+                from: ownerAddress,
+            });
+
+            await shiritori.setFirstWord("りんご", {
+                from: ownerAddress
+            });
+            history = await shiritori.history({
+                from: ownerAddress
+            });
+
+            assert.equal(history, firstWord, "don't set to the second word");
+
+        });
+    });
+
+    describe("when word is set by the not-owner", () => {
+        it("set first word", async () => {
+            const shiritori = await Shiritori.deployed()
+            try {
+                await shiritori.setFirstWord("しりとり", {
+                    from: notOwnerAddress
+                });
+            } catch (error) {
+                const errorMessage = "Ownable: caller is not the owner"
+                assert(error.reason, errorMessage, "error-reason is not expected");
+                return
+            }
+
+            assert.equal(false, "setFirstWord should not be called by non-owner");
+        });
+    });
+
+    describe("get shiritori history", () => {
+        const firstWord = "しりとり";
+        it("get first word by owner", async () => {
+            const shiritori = await Shiritori.deployed()
+            await shiritori.setFirstWord(firstWord, {
+                from: ownerAddress
+            });
+            await shiritori.history({
+                from: ownerAddress
+            });
+        })
+        it("get first word by not-owner", async () => {
+            const shiritori = await Shiritori.deployed()
+            await shiritori.setFirstWord(firstWord, {
+                from: ownerAddress
+            });
+            await shiritori.history({
+                from: notOwnerAddress
+            });
+        })
+
     });
 });
