e28611b79c35e7daa07dd9451851e2cea8c05665
TakuKitamura <takukitamura.io@gmail.com>
2022-04-03 12:28:53 +0900

    refacta deployment test

--- test/shiritori_test.js
+++ test/shiritori_test.js
@@ -5,9 +5,11 @@ const Shiritori = artifacts.require("Shiritori");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("ShiritoriTest", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Shiritori.deployed();
-    return assert.isTrue(true);
-  });
+contract("ShiritoriTest", function ( /* accounts */ ) {
+    describe("deployment", () => {
+        it("has been deployed successfully", async () => {
+            const shiritori = await Shiritori.deployed();
+            assert(shiritori, "contract failed to deploy");
+        });
+    });
 });
