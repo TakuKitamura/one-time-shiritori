89311b83512a397eb733b9b5b9a8b504020a6922
TakuKitamura <takukitamura.io@gmail.com>
2022-04-03 21:23:14 +0900

    gets string bytes-length function

--- contracts/Shiritori.sol
+++ contracts/Shiritori.sol
@@ -6,6 +6,11 @@ import "openzeppelin-solidity/contracts/access/Ownable.sol";
 contract Shiritori is Ownable {
     string private _history = "";
 
+    // get string bytes length
+    function getBytesLength(string memory str) private pure returns (uint256) {
+        return bytes(str).length;
+    }
+
     // get shiritori history
     function history() external view returns (string memory) {
         return _history;
@@ -13,7 +18,7 @@ contract Shiritori is Ownable {
 
     // contract-owner can set shiritori-history only once
     function setFirstWord(string calldata word) external onlyOwner {
-        bool isEmptyWord = bytes(_history).length == 0;
+        bool isEmptyWord = getBytesLength(_history) == 0;
         if (isEmptyWord) {
             // set first word on storage
             _history = word;
