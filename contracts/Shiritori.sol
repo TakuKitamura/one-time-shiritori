cbe419e024aa67b9f0b037e21ae38c2b3b090684
TakuKitamura <takukitamura.io@gmail.com>
2022-04-06 07:04:13 +0900

    change shiritori history variable to array

--- contracts/Shiritori.sol
+++ contracts/Shiritori.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract Shiritori is Ownable {
-    string private _history = "";
+    string[] private _history;
 
     // get string bytes length
     function getBytesLength(string memory str) private pure returns (uint256) {
@@ -12,24 +12,24 @@ contract Shiritori is Ownable {
     }
 
     // get shiritori history
-    function history() external view returns (string memory) {
+    function history() external view returns (string[] memory) {
         return _history;
     }
 
     // contract-owner can set shiritori-history only once
     function setFirstWord(string calldata word) external onlyOwner {
-        bool isEmptyHistory = getBytesLength(_history) == 0;
+        bool isEmptyHistory = _history.length == 0;
         if (isEmptyHistory) {
             // set first word on storage
-            _history = word;
+            _history.push(word);
         }
     }
 
     // update shiritori history
     function sayNextWord(string calldata word) external {
-        bool wordsAreSet = getBytesLength(_history) > 0;
+        bool wordsAreSet = _history.length > 0;
         if (wordsAreSet) {
-            _history = string(bytes.concat(bytes(_history), ",", bytes(word)));
+            _history.push(word);
         }
     }
 
